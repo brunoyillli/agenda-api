67487db77544607d5b183992bffdad177222a9de
Bruno Mendes <wolfteam1910@gmail.com>
2022-08-31 12:52:51 -0300

    habilitando cross para usar api no front

--- src/main/java/io/github/brunoyillli/agenda/api/rest/ContatoController.java
+++ src/main/java/io/github/brunoyillli/agenda/api/rest/ContatoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -21,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/contatos")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ContatoController {
 	
 	private final ContatoRepository repository;
