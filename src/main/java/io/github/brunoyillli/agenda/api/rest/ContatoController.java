3504dd0d7ac2d9df753812f05fa4b4dd2697fc32
Bruno Mendes <wolfteam1910@gmail.com>
2022-09-01 21:37:12 -0300

    ajustee

--- src/main/java/io/github/brunoyillli/agenda/api/rest/ContatoController.java
+++ src/main/java/io/github/brunoyillli/agenda/api/rest/ContatoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -44,7 +45,7 @@ public class ContatoController {
 		return repository.findAll();
 	}
 	
-	@PatchMapping("{id}/favorito")
+	@PutMapping("{id}/favorito")
 	public void favorite( @PathVariable Integer id ) {
 		Optional<Contato> contato = repository.findById(id);
 		contato.ifPresent(c -> {
